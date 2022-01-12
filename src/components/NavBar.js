68e194b5016489c65286689f4863cc29ac6b2292
santiagohsv <santiagosv@santiagos-MacBook-Pro.local>
2022-01-12 01:44:51 -0300

    navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="navBar">
+      <button>Inicio</button>
+      <button>Contacto</button>
+      <button>Tienda</button>
+    </nav>
+  );
+};
+
+export default NavBar;
