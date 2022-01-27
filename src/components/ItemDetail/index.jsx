d071489d6d02628039ce012e23d3cb89cc0aec29
santiagohsv <santiagosv@santiagos-MacBook-Pro.local>
2022-01-27 00:03:02 -0300

    Desafio clase 7

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,35 +1,27 @@
 import "./styles.css";
 
-
-
-const ItemDetail = ({item}) => {
-
-
+const ItemDetail = ({ item }) => {
   return (
     <>
+      <h1>{item.name}</h1>
+      <div className="itemDetailContainer">
+        <div className="imgContainer">
+          <img src={item.img} alt="{item.description}" />
+        </div>
 
-  
+        <div className="detailContainer">
+          <p className="itemStock">Stock: {item.stock} </p>
+          <p className="itemPrice">$ {item.precio}</p>
+          <h2>Caracteristicas</h2>
+          <li>{item.specs[0]}</li>
+          <li>{item.specs[1]}</li>
+          <li>{item.specs[2]}</li>
+          <li>{item.specs[3]}</li>
 
-    <h1>{item.name}</h1>
-    <div className="itemDetailContainer">
-    <div className="imgContainer">
-    <img src={item.img} alt="{item.description}"/>
-    </div>
-
-    <div className="detailContainer">
-        <p className="itemStock">Stock: {item.stock} </p>
-        <p className="itemPrice">$ {item.precio}</p>
-        <h2>Caracteristicas</h2>
-        <li>{item.specs[0]}</li>
-        <li>{item.specs[1]}</li>
-        <li>{item.specs[2]}</li>
-        <li>{item.specs[3]}</li> 
-
-        <div className="productDescription">{item.longDescription}</div>
-        <button>Agregar </button>
+          <div className="productDescription">{item.longDescription}</div>
+          <button>Agregar </button>
+        </div>
       </div>
-
-   </div>
     </>
   );
 };
