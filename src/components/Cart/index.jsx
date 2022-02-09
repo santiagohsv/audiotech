ee324be58a3963c14abd0f819b2b10a15fc018c9
santiagohsv <santiagosv@santiagos-MacBook-Pro.local>
2022-02-09 20:50:45 -0300

    Desafio clase 11 - ae eliminó código comentado

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -37,37 +37,3 @@ const Cart = () => {
 export default Cart
 
 
-/* import { useContext } from "react"
-import { CartContex } from "../../contexts/CartContext";
-import "./styles.css";
-
-const Cart = () => {
-  const { cart } = useContext(CartContex);
-
-  return (
-    <>
-      
-      {cart.map((items) => (
-        <div key={items.id} className="item-resume">
-          <div className="item-img">
-            <img src={items.img} alt="{items.description} " />
-          </div>
-          <div className="item-name">
-            <h3>{items.name}</h3>
-          </div>
-          <div className="item-qty">
-            <h3>{items.cantidad}</h3>
-          </div>
-          <div className="item-price">
-            <h3> $ {items.precio}</h3>
-          </div>
-          <button className="removeBtn">
-            Remove
-          </button>
-        </div>
-      ))}
-    </>
-  );
-};
-
-export default Cart */
